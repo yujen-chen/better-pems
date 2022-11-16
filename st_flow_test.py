766bfd5614da88d4f7e849a8589d19121a411e02
Yu-Jen Chen <chen.1571@osu.edu>
2022-11-16 15:45:45 -0800

    single-multiple-route draft

--- st_flow_test.py
+++ st_flow_test.py
@@ -10,14 +10,18 @@ from scipy import stats
 
 #######################################
 # Load the data
+
+# if the df-flow data exists, skip
 #######################################
-df_flow = (pd
- .read_csv('raw_data/d12_hr_MLHVflow_201902.csv', parse_dates=['timestamp'])
- .assign(timestamp=lambda df_: df_['timestamp'].dt.tz_localize(None))
- .assign(year=lambda df_: df_['timestamp'].dt.year,
-         month=lambda df_: df_['timestamp'].dt.month,
-         day=lambda df_: df_['timestamp'].dt.day,
-         hour=lambda df_: df_['timestamp'].dt.hour)
+df_flow = (
+    pd.read_csv("data/d12_hr_MLHVflow_201902.csv", parse_dates=["timestamp"])
+    .assign(timestamp=lambda df_: df_["timestamp"].dt.tz_localize(None))
+    .assign(
+        year=lambda df_: df_["timestamp"].dt.year,
+        month=lambda df_: df_["timestamp"].dt.month,
+        day=lambda df_: df_["timestamp"].dt.day,
+        hour=lambda df_: df_["timestamp"].dt.hour,
+    )
 )
 
 
@@ -26,52 +30,119 @@ df_flow = (pd
 #######################################
 @st.cache
 def route_df(route_num):
-    route_raw = df_flow[(df_flow.route==route_num)]
-    pivot_raw = pd.pivot_table(route_raw, values='total_flow', index=['station_id', 'abs_pm'], columns=['hour'], aggfunc=np.mean).sort_values('abs_pm')
-    route_raw_new = pivot_raw[(np.abs(stats.zscore(pivot_raw)) < 3)].dropna().reset_index()
-    route_flow_df = route_raw[route_raw['station_id'].isin(route_raw_new['station_id'])]
-    route_flow_df = route_flow_df.assign(day_of_week=lambda x: (x['timestamp'].dt.dayofweek))
-    route_flow_df['weekday'] = route_flow_df['day_of_week'].apply(lambda x: 'weekdays' if x < 6 else 'weekends')
+    route_raw = df_flow[(df_flow.route == route_num)]
+    pivot_raw = pd.pivot_table(
+        route_raw,
+        values="total_flow",
+        index=["station_id", "abs_pm"],
+        columns=["hour"],
+        aggfunc=np.mean,
+    ).sort_values("abs_pm")
+    route_raw_new = (
+        pivot_raw[(np.abs(stats.zscore(pivot_raw)) < 3)].dropna().reset_index()
+    )
+    route_flow_df = route_raw[route_raw["station_id"].isin(route_raw_new["station_id"])]
+    route_flow_df = route_flow_df.assign(
+        day_of_week=lambda x: (x["timestamp"].dt.dayofweek)
+    )
+    route_flow_df["weekday"] = route_flow_df["day_of_week"].apply(
+        lambda x: "weekdays" if x < 6 else "weekends"
+    )
     return route_flow_df
+
 
 @st.cache
 def weekday_pivot_df(route_df):
-    direction_a = route_df['direction'].unique()[0]
-    direction_b = route_df['direction'].unique()[1]
-    route_df_direction_a_pivot = pd.pivot_table(route_df[route_df['direction']==direction_a], values='total_flow', index=['hour'], columns=['weekday'], aggfunc=np.mean).reset_index()
-    route_df_direction_b_pivot = pd.pivot_table(route_df[route_df['direction']==direction_b], values='total_flow', index=['hour'], columns=['weekday'], aggfunc=np.mean).reset_index()
-    route_df_pivot = route_df_direction_a_pivot.add(route_df_direction_b_pivot, fill_value=0)
-    route_df_pivot['hour'] = route_df_pivot['hour']/2
+    direction_a = route_df["direction"].unique()[0]
+    direction_b = route_df["direction"].unique()[1]
+    route_df_direction_a_pivot = pd.pivot_table(
+        route_df[route_df["direction"] == direction_a],
+        values="total_flow",
+        index=["hour"],
+        columns=["weekday"],
+        aggfunc=np.mean,
+    ).reset_index()
+    route_df_direction_b_pivot = pd.pivot_table(
+        route_df[route_df["direction"] == direction_b],
+        values="total_flow",
+        index=["hour"],
+        columns=["weekday"],
+        aggfunc=np.mean,
+    ).reset_index()
+    route_df_pivot = route_df_direction_a_pivot.add(
+        route_df_direction_b_pivot, fill_value=0
+    )
+    route_df_pivot["hour"] = route_df_pivot["hour"] / 2
     route_df_pivot = route_df_pivot.round(0)
     return route_df_pivot
 
 
-
 #######################################
-#Start building Streamlit App
+# Start building Streamlit App
 #######################################
 
+single_multiple_sidebar = st.sidebar.selectbox(
+    "Single or Multiple Route", ("Single", "Multiples")
+)
 
-routes_number_list = df_flow['route'].unique()
-routes_number_list = list(routes_number_list)
+routes_number_list = df_flow["route"].unique()
+routes_number_list = np.sort(routes_number_list)
 # routes_number_list.insert(0, 'Select the route.')
 
-
 # streamlit sidebar
-route_number = st.sidebar.selectbox("Choose the route", (routes_number_list))
 
-route_num_df = route_df(route_number)
-route_flow_df = weekday_pivot_df(route_num_df)
+if single_multiple_sidebar == "Single":
 
-st.write(f"{route_number} is selected!")
-st.dataframe(route_flow_df.style.format("{:.0f}"))
+    route_number = st.sidebar.selectbox("Choose the route", (routes_number_list))
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=route_flow_df['hour'], y=route_flow_df['weekdays'], name='weekday flow', line=dict(color='red', width=4)))
-fig.add_trace(go.Scatter(x=route_flow_df['hour'], y=route_flow_df['weekends'], name='weekend flow', line=dict(color='royalblue', width=4)))
-# fig = px.line(route_flow_df, x='hour', y=route_flow_df.columns, range_x=(-0.5, 23.5), title=f"Weekday vs. Weekend Avg. Hourly Flow for Route {route_number}", markers=True)
-fig.update_yaxes(minor_ticks="inside")
-fig.update_layout(title=f"Weekday vs. Weekend Avg. Hourly Flow for Route {route_number}",
-                   xaxis_title='Hour',
-                   yaxis_title='Avg. Flow')
-st.plotly_chart(fig, use_container_width=True)
+    route_num_df = route_df(route_number)
+    route_flow_df = weekday_pivot_df(route_num_df)
+
+    st.write(f"{route_number} is selected!")
+    st.dataframe(route_flow_df.style.format("{:.0f}"))
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=route_flow_df["hour"],
+            y=route_flow_df["weekdays"],
+            name="weekday flow",
+            line=dict(color="royalblue", width=4),
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=route_flow_df["hour"],
+            y=route_flow_df["weekends"],
+            name="weekend flow",
+            line=dict(color="lime", width=4),
+        )
+    )
+    # fig = px.line(route_flow_df, x='hour', y=route_flow_df.columns, range_x=(-0.5, 23.5), title=f"Weekday vs. Weekend Avg. Hourly Flow for Route {route_number}", markers=True)
+    fig.update_yaxes(minor_ticks="inside")
+    fig.update_layout(
+        title=f"Weekday vs. Weekend Avg. Hourly Flow for Route {route_number}",
+        xaxis_title="Hour",
+        yaxis_title="Avg. Flow",
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+if single_multiple_sidebar == "Multiples":
+    weekday_weekends_sidebar = st.sidebar.selectbox(
+        "Weekdays or Weekends", ("weekday", "weekends")
+    )
+    st.write("Multiple Routes Selected")
+    all_pivot_df = pd.read_csv("all_pivot_df.csv")
+    # st.dataframe(all_pivot_df)
+    # st.write(routes_number_list)
+    fig = go.Figure()
+    pivot_df_list = []
+    for i in routes_number_list:
+        pivot_df = all_pivot_df[all_pivot_df["route"] == i]
+        # st.dataframe(pivot_df)
+        fig.add_trace(
+            go.Scatter(
+                x=pivot_df["hour"], y=pivot_df[weekday_weekends_sidebar], name=str(i)
+            )
+        )
+    st.plotly_chart(fig)
